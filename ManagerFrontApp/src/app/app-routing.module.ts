d04f31107965a01d4ce370ac6b85c3dd33941424
ljuti21 <prodanovic.ra34.2018@uns.ac.rs>
2021-11-01 16:35:46 +0100

    feat: added front page and minor changes in already done work
    Added demo front page to ease colaboration with other teams.

--- ManagerFrontApp/src/app/app-routing.module.ts
+++ ManagerFrontApp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuildingsMapComponent } from './buildings-map/buildings-map.component';
 import { Hospital1Component } from './hospital1/hospital1.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 const routes: Routes = [
-{path: "", component: BuildingsMapComponent},
+{path: "", component: FrontPageComponent},
+{path:"buildings", component: BuildingsMapComponent},
 {path:"hospital1", component: Hospital1Component}
 ];
 
--- ManagerFrontApp/src/app/app.module.ts
+++ ManagerFrontApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuildingsMapComponent } from './buildings-map/buildings-map.component';
 import { Hospital1Component } from './hospital1/hospital1.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuildingsMapComponent,
-    Hospital1Component
+    Hospital1Component,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
