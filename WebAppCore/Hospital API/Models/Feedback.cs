ddef1b4db89bf534ea6f8355e7bbfcfaf0a9422c
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-11-01 21:27:33 +0100

    feat:weathercontroller

--- WebAppCore/Hospital API/Models/Feedback.cs
+++ WebAppCore/Hospital API/Models/Feedback.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hospital_API.Models
 {
+    [Table("Feedbacks",Schema="public")]
     public class Feedback
     {
         public int Id { get; set; }
