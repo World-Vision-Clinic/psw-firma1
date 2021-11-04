e4a6b962768ba295f4ed3ebe55340644b307d93f
Mihajlo Marjanski <marjanski.mihajlo@uns.ac.rs>
2021-11-04 20:37:50 +0100

    feat: Added database query, proxy config, error handle on front

--- patient-portal-front/src/app/patient-feedback-service.service.ts
+++ patient-portal-front/src/app/patient-feedback-service.service.ts
@@ -8,12 +8,11 @@ import { IFeedback } from './feedback';
 })
 export class PatientFeedbackServiceService {
 
-  private _url: string ="http://localhost:39901/api/Feedbacks"
 
   constructor(private http: HttpClient) { }
 
 
-  getFeedback2() : Observable<IFeedback[]>{
-    return this.http.get<IFeedback[]>(this._url)
+  getFeedback() : Observable<IFeedback[]>{
+    return this.http.get<IFeedback[]>("/api/Feedbacks/published")
   }
 }
--- patient-portal-front/src/app/patient-feedback-view/patient-feedback-view.component.ts
+++ patient-portal-front/src/app/patient-feedback-view/patient-feedback-view.component.ts
@@ -5,16 +5,19 @@ import { PatientFeedbackServiceService } from '../patient-feedback-service.servi
   selector: 'app-patient-feedback-view',
   template: `
         <h2>Hospital Feedback</h2>
-        <div *ngFor = "let f of feedback">
-          <hr>
-          <div *ngIf="f.isPublic">
+        <span *ngIf="errorMsg; then printError else printFeedback"></span>
+        <ng-template #printError>{{errorMsg}}</ng-template>
+        <ng-template #printFeedback>
+          <div *ngFor = "let f of feedback">
+            <hr>
             {{f.content}}<br>
             <span *ngIf="f.isAnonymous; then thenBlock else elseBlock"></span>
             <ng-template #thenBlock>by Anonymous</ng-template>
             <ng-template #elseBlock>by User</ng-template>
-          </div>
-        </div> 
-        <hr>  
+          </div> 
+          <hr>  
+        </ng-template>
+
   
   `,
   styleUrls: ['./patient-feedback-view.component.css']
@@ -22,11 +25,13 @@ import { PatientFeedbackServiceService } from '../patient-feedback-service.servi
 export class PatientFeedbackViewComponent implements OnInit {
 
   public feedback = [] as any
+  public errorMsg = ""
 
   constructor(private _patientService : PatientFeedbackServiceService) { }
 
   ngOnInit(): void {
-    this._patientService.getFeedback2().subscribe(data => this.feedback = data);
+    this._patientService.getFeedback().subscribe(data => this.feedback = data,
+                                                error => this.errorMsg = "Couldn't load user feedback");
   }
 
 }
