9311122f3c2a63fb780c29f36d6fdd4072fad447
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2021-11-06 03:06:27 +0100

    feat:manager view css

--- ManagerFrontApp/src/app/app-routing.module.ts
+++ ManagerFrontApp/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { Hospital1Component } from './hospital1/hospital1.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { PharmacyRegistrationComponent } from './pharmacy-registration/pharmacy-registration.component';
 import { ManagerObjectionsComponent } from './manager-objections/manager-objections.component';
+import { ManagerFeedbackViewComponent } from './manager-feedback-view/manager-feedback-view.component';
 
 const routes: Routes = [
 {path: "", component: FrontPageComponent},
 {path:"buildings", component: BuildingsMapComponent},
 {path:"hospital1", component: Hospital1Component},
 {path:"pharmacy-registration", component: PharmacyRegistrationComponent},
-{path:"manager-objections", component: ManagerObjectionsComponent}
+{path:"manager-objections", component: ManagerObjectionsComponent},
+{path:"manager-feedback", component: ManagerFeedbackViewComponent}
 ];
 
 @NgModule({
