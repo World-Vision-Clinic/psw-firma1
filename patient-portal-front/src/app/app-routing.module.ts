d84fb8eca0c33ba6a0c59b9a239818a10ffd421c
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-11-06 12:17:25 +0100

    feat: Routing

--- patient-portal-front/src/app/app-routing.module.ts
+++ patient-portal-front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientCreateFeedbackComponent } from './patient-create-feedback/patient-create-feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "create-feedback", component: PatientCreateFeedbackComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
