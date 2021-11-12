9baf6bfc5268a6ae95d93eac4ab3ba1c1560916f
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2021-11-12 16:44:42 +0100

    refactor: deleting old pharmacy

--- PharmacyApp/src/main/java/com/pharmacy/repository/ObjectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharmacy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pharmacy.model.Objection;
-
-public interface ObjectionRepository extends JpaRepository<Objection, Integer>{
-
-}
