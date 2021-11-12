97dd139a26924aed97343591eb5c0d628743b3b1
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2021-11-12 08:12:16 +0100

    feat: added tests for patient survey

---
+++ WebAppCore/TestPatientPortal/PatientSurvey/TestSurveyModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPatientPortal
+{
+    class TestSurveyModel
+    {
+    }
+}
---
+++ WebAppCore/TestPatientPortal/PatientSurvey/TestSurveySectionService.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPatientPortal.PatientSurvey
+{
+    class TestSurveySectionService
+    {
+        [Test]
+        public void TestAddSurveySectionService()
+        {
+            Assert.Pass();
+        }
+    }
+}
