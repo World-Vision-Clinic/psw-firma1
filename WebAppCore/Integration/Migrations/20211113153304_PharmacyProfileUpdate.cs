c8b22763ec591e8fa115a8fd00d121693e3c5189
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2021-11-13 16:47:02 +0100

    refactor: address and city added to pharmacy

---
+++ WebAppCore/Integration/Migrations/20211113153304_PharmacyProfileUpdate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integration.Migrations
+{
+    public partial class PharmacyProfileUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Pharmacies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Pharmacies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Pharmacies");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Pharmacies");
+        }
+    }
+}
