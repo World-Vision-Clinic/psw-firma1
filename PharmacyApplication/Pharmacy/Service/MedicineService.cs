8d50928845fe441323749cd9cc7ae962dea6a68a
sanjadrinic <sanja.drinic@uns.ac.rs>
2021-11-16 10:17:28 +0100

    feat: implemented model and service for medicines

---
+++ PharmacyApplication/Pharmacy/Service/MedicineService.cs
@@ -0,0 +1,28 @@
+﻿using Pharmacy.Model;
+using Pharmacy.Repository.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Service
+{
+    public class MedicineService
+    {
+        IMedicineRepository repository;
+
+        public MedicineService(IMedicineRepository medicineRepository)
+        {
+            repository = medicineRepository;
+        }
+
+        public List<Medicine> GetAll() 
+        {
+            return repository.GetAll();
+        }
+
+        public Medicine GetById(long medicineId) 
+        {
+            return repository.GetById(medicineId);
+        }
+    }
+}
