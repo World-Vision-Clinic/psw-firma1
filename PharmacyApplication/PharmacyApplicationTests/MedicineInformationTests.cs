7638da77e4de58e76416da4e8db73876dad3d579
sanjadrinic <sanja.drinic@uns.ac.rs>
2021-11-17 15:15:29 +0100

    feat: find substitute medicines
    Unit test for finding all substitute medicines

--- PharmacyApplication/PharmacyApplicationTests/MedicineInformationTests.cs
+++ PharmacyApplication/PharmacyApplicationTests/MedicineInformationTests.cs
@@ -17,5 +17,29 @@ namespace PharmacyApplicationTests
             Medicine medicine = service.GetById(1L);
             Assert.NotNull(medicine);
         }
+
+        [Fact]
+        public void Find_substitute_medicines()
+        {
+            List<long> expectedSubstituteMedicinesId = new List<long>();
+            expectedSubstituteMedicinesId.Add(2L);
+
+            MedicineService service = new MedicineService(new MedicineRepository());
+
+            Medicine medicine = service.GetById(1L);
+            List<long> actualSubstituteMedicinesId = FindAllSubstituteMedicinesId(medicine);
+
+            actualSubstituteMedicinesId.ShouldBeEquivalentTo(expectedSubstituteMedicinesId);
+        }
+
+        private static List<long> FindAllSubstituteMedicinesId(Medicine medicine)
+        {
+            List<long> actualSubstituteMedicinesId = new List<long>();
+            foreach (SubstituteMedicine actualSubstituteMedicine in medicine.SubstituteMedicines)
+            {
+                actualSubstituteMedicinesId.Add(actualSubstituteMedicine.SubstituteId);
+            }
+            return actualSubstituteMedicinesId;
+        }
     }
 }
