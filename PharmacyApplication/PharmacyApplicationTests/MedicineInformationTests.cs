66b018b45b604bbc089d02a6fda2ec61d9cd6200
sanjadrinic <sanja.drinic@uns.ac.rs>
2021-11-17 15:07:46 +0100

    feat: added initial pharmacy testing project

---
+++ PharmacyApplication/PharmacyApplicationTests/MedicineInformationTests.cs
@@ -0,0 +1,15 @@
+using Pharmacy.Model;
+using Pharmacy.Repository;
+using Pharmacy.Service;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PharmacyApplicationTests
+{
+    public class MedicineInformationTests
+    {
+       
+    }
+}
