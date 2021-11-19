756e0fb6ae4a6062aacf20fe569b9f153290e41c
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2021-11-19 13:21:43 +0100

    feat:specification file upload enabled

--- PharmacyApplication/PharmacyApplication/Controller/MedicinesController.cs
+++ PharmacyApplication/PharmacyApplication/Controller/MedicinesController.cs
@@ -4,8 +4,10 @@ using Pharmacy.Model;
 using Pharmacy.Repository;
 using Pharmacy.Service;
 using PharmacyAPI.Dto;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -56,6 +58,67 @@ namespace PharmacyAPI.Controller
             }
 
             return Ok();
+        }
+
+        [HttpGet("spec")]
+        public IActionResult GetMedicineSpecification(string name = "")
+        {
+            if (!Request.Headers.TryGetValue("ApiKey", out var extractedApiKey))
+            {
+                return BadRequest("Api Key was not provided");
+            }
+
+            Hospital hospital = hospitalService.GetHospitalByApiKey(extractedApiKey);
+            if (hospital == null)
+            {
+                return BadRequest("Api Key is not valid!");
+            }
+
+            if (name.Length <= 0)
+            {
+                return BadRequest();
+            }
+
+            name = name.Substring(0, 1).ToUpper() + name.Substring(1, name.Length-1);
+            List<Medicine> medicines = service.GetByName(name);
+            if (medicines.Count <= 0)
+            {
+                return BadRequest("Medicine don't exist");
+            }
+
+            foreach(Medicine medicine in medicines)
+            {
+                createFile(service.GetSpecification(medicine));
+                break;
+            }
+
+            uploadSpecification();
+
+            return Ok();
+        }
+
+        private void createFile(string specification)
+        {
+            StreamWriter file = new StreamWriter("Specification.txt");
+            file.Write(specification);
+            file.Close();
+        }
+
+        private void uploadSpecification()
+        {
+            using (SftpClient client = new SftpClient(new PasswordConnectionInfo("192.168.0.21", "user", "password")))
+            {
+                client.Connect();
+
+                string sourceFile = @"Specification.txt";
+                using (Stream stream = System.IO.File.OpenRead(sourceFile))
+                {
+                    client.UploadFile(stream, @"\public\" + Path.GetFileName(sourceFile));
+                }
+                
+                client.Disconnect();
+            }
+
         }
     }
 }
