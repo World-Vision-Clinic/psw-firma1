dbb844bc779a9c2f3fc45bee6f11303efb17f018
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2021-11-19 14:01:21 +0100

    feat/ save specification file on hospital side enabled

--- WebAppCore/Integration API/Controller/MedicinesController.cs
+++ WebAppCore/Integration API/Controller/MedicinesController.cs
@@ -5,9 +5,11 @@ using Integration_API.Dto;
 using Integration_API.Mapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Renci.SshNet;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -42,7 +44,7 @@ namespace Integration_API.Controller
 
             foreach(PharmacyProfile pharmacy in pharmaciesService.GetAll())
             {
-                if (SendRequest(pharmacy, medicineDto))
+                if (SendRequestToCheckAvailability(pharmacy, medicineDto))
                 {
                     pharmaciesWithMedicine.Add(PharmacyMapper.PharmacyToPharmacyDto(pharmacy));
                 }
@@ -51,7 +53,73 @@ namespace Integration_API.Controller
             return Ok(pharmaciesWithMedicine);
         }
 
-        private bool SendRequest(PharmacyProfile pharmacy, MedicineDto medicineDto)
+        [HttpGet("spec")]
+        public IActionResult GetSpecification(string pharmacyLocalhost = "", string medicine = "")
+        {
+            if (pharmacyLocalhost.Length <= 0 || medicine.Length <= 0)
+            {
+                return BadRequest();
+            }
+
+            if(!SendRequestForSpecification(pharmacyLocalhost, medicine))
+            {
+                return BadRequest();
+            }
+
+            if (!DownloadSpecification())
+            {
+                return BadRequest("Unable to download specification file");
+            }
+
+            return Ok();
+        }
+
+        private bool SendRequestForSpecification(string pharmacyLocalhost, string medicineName)
+        {
+            var client = new RestSharp.RestClient(pharmacyLocalhost);
+            var request = new RestRequest("/medicines/spec?name=" + medicineName);
+
+            Credential credential = credentialsService.GetByPharmacyLocalhost(pharmacyLocalhost);
+
+            request.AddHeader("ApiKey", credential.ApiKey);
+
+            IRestResponse response = client.Get(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DownloadSpecification()
+        {
+            try
+            {
+                using (SftpClient client = new SftpClient(new PasswordConnectionInfo("192.168.0.21", "user", "password")))
+                {
+                    client.Connect();
+
+                    string serverFile = @"\public\Specification.txt";
+                    string localFile = @"Specification.txt";
+                    using (Stream stream = System.IO.File.OpenWrite(localFile))
+                    {
+                        client.DownloadFile(serverFile, stream);
+                    }
+
+                    client.Disconnect();
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool SendRequestToCheckAvailability(PharmacyProfile pharmacy, MedicineDto medicineDto)
         {
             var client = new RestSharp.RestClient(pharmacy.Localhost);
             var request = new RestRequest("/medicines/check?name=" + medicineDto.Name + "&dosage=" + medicineDto.DosageInMg + "&quantity=" + medicineDto.Quantity);
