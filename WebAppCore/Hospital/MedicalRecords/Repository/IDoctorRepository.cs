6e604312500a1dea1b25e35a99531f899c2112f4
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2021-11-23 00:10:31 +0100

    feat:doctor repository

---
+++ WebAppCore/Hospital/MedicalRecords/Repository/IDoctorRepository.cs
@@ -0,0 +1,21 @@
+﻿using Hospital.MedicalRecords.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.MedicalRecords.Repository
+{
+    public interface IDoctorRepository
+    {
+        public void AddDoctor(Doctor doctor);
+
+        public void SaveSync();
+
+        public Doctor FindById(int id);
+
+        public void Modify(Doctor doctor);
+        public List<Doctor> GetDoctorsFromList(List<int> doctorIds);
+
+        public List<Doctor> GetAvailableDoctors();
+    }
+}
