3d5a5ee89cb33351c688c8039a8704f66122b116
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2021-11-22 19:55:30 +0100

    teast: updated test for search by name

--- PharmacyApplication/PharmacyApplicationTests/UnitTests/SearchTests.cs
+++ PharmacyApplication/PharmacyApplicationTests/UnitTests/SearchTests.cs
@@ -12,6 +12,7 @@ namespace PharmacyApplicationTests.UnitTests
     public class SarchTest
     {
         MedicineService service;
+        SubstanceService substanceService;
 
         [Fact]
         public void Search_medicine_by_name()
@@ -25,21 +26,29 @@ namespace PharmacyApplicationTests.UnitTests
 
             List<Medicine> ret = service.GetByName("Fervex");
 
-            Assert.Contains(medicines, item => item.MedicineId == 55);
+            Assert.Contains(ret, item => item.MedicineId == 55);
         }
+        //[Fact]
+        //public void Search_medicine_by_substance()
+        //{
+        //    var stubRepositoryM = new Mock<IMedicineRepository>();
+        //    var stubRepositoryS = new Mock<ISubstanceRepository>();
+        //    service = new MedicineService(stubRepositoryM.Object);
+        //    substanceService = new SubstanceService(stubRepository.Object);
+        //    List<Medicine> medicines = new List<Medicine>(); 
+        //    List<Substance> substances = new List<Substance>();
+        //    Medicine medicine = new Medicine(55, "Fervex", "", "", "", new List<SubstituteMedicine>(), 250, "", "", substances, 213);
+        //    Substance sub = new Substance(33, "s1", 213, 55);
+        //    substances.Add(sub);
+        //    stubRepositoryM.Setup(s => s.GetAll()).Returns(substances);
+        //    medicine.Substances.Add(s);
+        //    medicines.Add(medicine);
+        //    stubRepositoryM.Setup(m => m.GetAll()).Returns(medicines);
 
-        public void Search_medicine_by_substance()
-        {
-            var stubRepository = new Mock<IMedicineRepository>();
-            service = new MedicineService(stubRepository.Object);
-            List<Medicine> medicines = new List<Medicine>();
-            Medicine medicine = new Medicine(55, "Fervex", "", "", "", new List<SubstituteMedicine>(), 250, "", "", new List<Substance>(), 213);
-            medicines.Add(medicine);
-            stubRepository.Setup(m => m.GetAll()).Returns(medicines);
+        //    List<long> ret = substanceService.GetMedicineIdsBySubstance(33);
+        //    List<Medicine> retMed = service.ConvertIdsToMedicines(ret);
 
-            List<Medicine> ret = service.GetByName("Fervex");
-
-            Assert.Contains(medicines, item => item.MedicineId == 55);
-        }
+        //    Assert.Contains(retMed, item => item.MedicineId == 55);
+        //}
     }
 }
