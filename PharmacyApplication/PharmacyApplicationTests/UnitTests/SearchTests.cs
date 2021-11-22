c21ba86b369325b78bcf69288f4106a64f2bbc97
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2021-11-22 20:16:29 +0100

    fix: deleted unused methods

--- PharmacyApplication/PharmacyApplicationTests/UnitTests/SearchTests.cs
+++ PharmacyApplication/PharmacyApplicationTests/UnitTests/SearchTests.cs
@@ -28,27 +28,6 @@ namespace PharmacyApplicationTests.UnitTests
 
             Assert.Contains(ret, item => item.MedicineId == 55);
         }
-        //[Fact]
-        //public void Search_medicine_by_substance()
-        //{
-        //    var stubRepositoryM = new Mock<IMedicineRepository>();
-        //    var stubRepositoryS = new Mock<ISubstanceRepository>();
-        //    service = new MedicineService(stubRepositoryM.Object);
-        //    substanceService = new SubstanceService(stubRepository.Object);
-        //    List<Medicine> medicines = new List<Medicine>(); 
-        //    List<Substance> substances = new List<Substance>();
-        //    Medicine medicine = new Medicine(55, "Fervex", "", "", "", new List<SubstituteMedicine>(), 250, "", "", substances, 213);
-        //    Substance sub = new Substance(33, "s1", 213, 55);
-        //    substances.Add(sub);
-        //    stubRepositoryM.Setup(s => s.GetAll()).Returns(substances);
-        //    medicine.Substances.Add(s);
-        //    medicines.Add(medicine);
-        //    stubRepositoryM.Setup(m => m.GetAll()).Returns(medicines);
 
-        //    List<long> ret = substanceService.GetMedicineIdsBySubstance(33);
-        //    List<Medicine> retMed = service.ConvertIdsToMedicines(ret);
-
-        //    Assert.Contains(retMed, item => item.MedicineId == 55);
-        //}
     }
 }
