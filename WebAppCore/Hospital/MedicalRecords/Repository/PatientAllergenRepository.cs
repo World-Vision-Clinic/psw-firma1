0f2823f443455ba826486ac5a2b2c76a331dab14
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2021-11-23 21:26:00 +0100

    feat:patient allergen

---
+++ WebAppCore/Hospital/MedicalRecords/Repository/PatientAllergenRepository.cs
@@ -0,0 +1,76 @@
+﻿using Hospital.MedicalRecords.Model;
+using Hospital.SharedModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hospital.MedicalRecords.Repository
+{
+    public class PatientAllergenRepository : IPatientAllergenRepository
+    {
+        private readonly HospitalContext _context;
+        private readonly IPatientRepository _patientRepository;
+        private readonly IAllergenRepository _allergenRepository;
+
+        public PatientAllergenRepository()
+        {
+        }
+
+        public PatientAllergenRepository(HospitalContext context, IPatientRepository patientRepository, IAllergenRepository allergenRepository)
+        {
+            _context = context;
+            _patientRepository = patientRepository;
+            _allergenRepository = allergenRepository;
+        }
+
+        public void SaveSync()
+        {
+            _context.SaveChanges();
+        }
+
+        public void AddPatientAllergen(int patientId, int allergenId)
+        {
+            PatientAllergen patientAllergen = new PatientAllergen(patientId, allergenId);
+            _context.PatientAllergens.Add(patientAllergen);
+            SaveSync();
+        }
+
+        public PatientAllergen FindById(int id)
+        {
+            return _context.PatientAllergens.FirstOrDefault(d => d.Id == id);
+        }
+
+        public List<Allergen> FindByPatientId(int patientId)
+        {
+            var query = from pa in _context.PatientAllergens
+                        where pa.PatientId == patientId
+                        select new
+                        {
+                            allergenId = pa.AllergenId
+                        };
+            List<Allergen> allergens = new List<Allergen>();
+            foreach(var a in query)
+            {
+                allergens.Add(_allergenRepository.FindById(a.allergenId));
+            }
+            return allergens;
+        }
+
+        public List<Patient> FindByAllergenId(int allergenId)
+        {
+            var query = from pa in _context.PatientAllergens
+                        where pa.AllergenId == allergenId
+                        select new
+                        {
+                            patientId = pa.PatientId
+                        };
+            List<Patient> patients = new List<Patient>();
+            foreach (var a in query)
+            {
+                patients.Add(_patientRepository.FindById(a.patientId));
+            }
+            return patients;
+        }
+    }
+}
---
+++ WebAppCore/Hospital/MedicalRecords/Service/PatientAllergenService.cs
@@ -0,0 +1,43 @@
+﻿using Hospital.MedicalRecords.Model;
+using Hospital.MedicalRecords.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.MedicalRecords.Service
+{
+    public class PatientAllergenService
+    {
+        private readonly IPatientAllergenRepository _repo;
+
+        public PatientAllergenService(IPatientAllergenRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void AddPatientAllergen(int patientId, int allergenId)
+        {
+            _repo.AddPatientAllergen(patientId, allergenId);
+        }
+
+        public PatientAllergen FindById(int id)
+        {
+            return _repo.FindById(id);
+        }
+
+        public List<Allergen> FindByPatientId(int patientId)
+        {
+            return _repo.FindByPatientId(patientId);
+        }
+        public List<Patient> FindByAllergenId(int allergenId)
+        {
+            return _repo.FindByAllergenId(allergenId);
+        }
+
+        public void SaveSync()
+        {
+            _repo.SaveSync();
+        }
+
+    }
+}
