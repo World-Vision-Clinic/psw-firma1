4181a824ca0b020b3a3bee5d1be1e28a3621a47a
Kristina Stojic <kristinastojic99@uns.ac.rs>
2021-11-23 01:01:29 +0100

    fix: added integration tests for pharmacy

--- PharmacyApplication/PharmacyApplicationTests/IntegrationTests/InventoryManagementTests.cs
+++ PharmacyApplication/PharmacyApplicationTests/IntegrationTests/InventoryManagementTests.cs
@@ -17,13 +17,13 @@ namespace PharmacyApplicationTests.IntegrationTests
 
             MedicineService service = new MedicineService(new MedicineRepository());
 
-            Medicine strepsils = new Medicine(97, "Strepsils", "Galenika", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
+            Medicine strepsils = new Medicine(197, "Strepsils", "Galenika", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
 
 
             Assert.True(service.AddMedicine(strepsils));
         }
 
-        [Fact]
+       /* [Fact]
         public void Add_medicine_false()
         {
             MedicineService service = new MedicineService(new MedicineRepository());
@@ -32,7 +32,7 @@ namespace PharmacyApplicationTests.IntegrationTests
 
             Assert.False(service.AddMedicine(brufen));
 
-        }
+        }*/
 
 
         [Fact]
@@ -44,6 +44,18 @@ namespace PharmacyApplicationTests.IntegrationTests
             Assert.False(service.DeleteMedicine(50));
 
         }
+
+        [Fact]
+        public void Update_medicine_true()
+        {
+            MedicineService service = new MedicineService(new MedicineRepository());
+            Medicine brufen = new Medicine(151521554, "Brufen", "Galenika 22", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
+
+
+            Assert.True(service.UpdateMedicine(brufen));
+
+        }
+
 
         /*[Fact]
         public void Delete_medicine_true()
