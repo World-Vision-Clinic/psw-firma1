59563891ed5fa7244743d0332b1c32d59e6f1a4d
Kristina Stojic <kristinastojic99@uns.ac.rs>
2021-11-23 01:09:00 +0100

    fix: added integration tests for pharmacy

--- PharmacyApplication/PharmacyApplicationTests/IntegrationTests/InventoryManagementTests.cs
+++ PharmacyApplication/PharmacyApplicationTests/IntegrationTests/InventoryManagementTests.cs
@@ -12,27 +12,26 @@ namespace PharmacyApplicationTests.IntegrationTests
     {
 
         [Fact]
+        public void Update_medicine_true()
+        {
+            MedicineService service = new MedicineService(new MedicineRepository());
+            Medicine brufen = new Medicine(151521554, "Brufen", "Galenika 22", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
+
+            Assert.True(service.UpdateMedicine(brufen));
+
+        }
+
+        [Fact]
         public void Add_medicine_true()
         {
 
             MedicineService service = new MedicineService(new MedicineRepository());
 
-            Medicine strepsils = new Medicine(197, "Strepsils", "Galenika", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
+            Medicine strepsils = new Medicine(201, "Strepsils", "Galenika", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
 
 
             Assert.True(service.AddMedicine(strepsils));
         }
-
-       /* [Fact]
-        public void Add_medicine_false()
-        {
-            MedicineService service = new MedicineService(new MedicineRepository());
-
-            Medicine brufen = new Medicine(151521554, "Brufen", "Galenika", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
-
-            Assert.False(service.AddMedicine(brufen));
-
-        }*/
 
 
         [Fact]
@@ -44,27 +43,6 @@ namespace PharmacyApplicationTests.IntegrationTests
             Assert.False(service.DeleteMedicine(50));
 
         }
-
-        [Fact]
-        public void Update_medicine_true()
-        {
-            MedicineService service = new MedicineService(new MedicineRepository());
-            Medicine brufen = new Medicine(151521554, "Brufen", "Galenika 22", "sideEffects1", "usage1", new List<SubstituteMedicine>(), 200.00, "mainPrecautions1", "potentialDangers1", null, 100);
-
-
-            Assert.True(service.UpdateMedicine(brufen));
-
-        }
-
-
-        /*[Fact]
-        public void Delete_medicine_true()
-        {
-            MedicineService service = new MedicineService(new MedicineRepository());
-
-            Assert.True(service.DeleteMedicine(1));
-
-        }*/
 
 
     }
