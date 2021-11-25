c3a50e22ae16ef5777725d7eb2ede975758f9dad
prodanovic21 <73432423+ljuti21@users.noreply.github.com>
2021-11-25 01:05:10 +0100

    Feat/editor tests (#93)
    * feat: Testing of service, controller and repositories
    
    * fix: repacking of classes
    
    Changed destination of classes for reason of better organisation.
    
    * feat: Integration test
    
    Relocation of equipment from room to room.
    
    * fix: Fixed conflicts
    
    * refactor: Renaming test methods and classes
    
    * fix: unit test for building service
    
    * fix: unit tests
    
    * fix: testing data
    
    Co-authored-by: p-anja <73345582+p-anja@users.noreply.github.com>

---
+++ WebAppCore/HospitalTests/EditorTests/UnitTests/EquipmentRepositoryUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using Hospital.RoomsAndEquipment.Model;
+using Hospital.RoomsAndEquipment.Repository;
+using Hospital.SharedModel;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HospitalTests.EditorTests
+{
+    public class EquipmentRepositoryUnitTest
+    {
+        [Fact]
+        public void equipment_repository_test_1()
+        {
+            var options = new DbContextOptionsBuilder<HospitalContext>()
+                .UseInMemoryDatabase(databaseName: "HospitalDB")
+                .Options;
+
+
+            using (var context = new HospitalContext(options))
+            {
+                Equipment eq1 = new Equipment { id = 89, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 5, RoomId = 1 };
+                context.AllEquipment.Add(eq1);
+                context.SaveChanges();
+            }
+            using (var context = new HospitalContext(options))
+            {
+                EquipmentRepository eqRepository = new EquipmentRepository(context);
+                List<Equipment> eqpmnt = eqRepository.GetAll();
+
+                Assert.Equal(2, eqpmnt.Count);
+            }
+        }
+
+        [Fact]
+        public void equipment_repository_test_2()
+        {
+            var options = new DbContextOptionsBuilder<HospitalContext>()
+                .UseInMemoryDatabase(databaseName: "HospitalDB")
+                .Options;
+
+            using (var context = new HospitalContext(options))
+            {
+                Equipment eq1 = new Equipment { id = 1, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 5, RoomId = 1 };
+                context.AllEquipment.Add(eq1);
+                context.SaveChanges();
+                EquipmentRepository eqRepository = new EquipmentRepository(context);
+                Equipment eqpmntID = eqRepository.GetByID(1);
+
+                eqpmntID.id.ShouldBeEquivalentTo(1);
+            }
+        }
+    }
+}
