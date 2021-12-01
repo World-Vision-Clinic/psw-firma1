02c7c729c95450d2895aa255a54577ae169a5052
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-12-01 13:38:19 +0100

    feat: Blank free appointment generator backend

--- WebAppCore/Hospital/Schedule/Service/AppointmentService.cs
+++ WebAppCore/Hospital/Schedule/Service/AppointmentService.cs
@@ -8,6 +8,12 @@ using System.Text;
 
 namespace Hospital.Schedule.Service
 {
+    public enum AppointmentSearchPriority
+    {
+        NO_PRIORITY,
+        DOCTOR_PRIORITY,
+        DATE_TIME_PRIORITY
+    }
     public class AppointmentService
     {
         private readonly IAppointmentRepository _repo;
@@ -44,6 +50,13 @@ namespace Hospital.Schedule.Service
                    .Where(g => DatesOverlap(g.Date, g.Time, date, time)).ToList().FirstOrDefault();
         }
 
+        public Appointment GetAvailableByDateRangeAndDoctor(DateTime lowerDateRange, DateTime upperDateRange, int doctorId, AppointmentSearchPriority priority = AppointmentSearchPriority.NO_PRIORITY)
+        {
+            /*List<Appointment> allAppointments = GetAll();
+            if(GetByDateAndDoctor())*/
+            return null;
+        }
+
         public bool DatesOverlap(DateTime firstDate, TimeSpan firstTimeSpan, DateTime secondDate, TimeSpan secondTimeSpan)
         {
             if (firstDate <= secondDate && firstDate + firstTimeSpan > secondDate)
@@ -55,6 +68,33 @@ namespace Hospital.Schedule.Service
             if (secondDate >= firstDate && secondDate < firstDate + firstTimeSpan)
                 return true;
             return false;
+        }
+
+        public List<Appointment> GenerateFreeAppointmentList(DateTime lowerDateRange, DateTime upperDateRange, TimeSpan lowerTimeRange, TimeSpan upperTimeRange, TimeSpan appointmentLength, bool includeWeekends = false)
+        {
+            List<Appointment> freeAppointmentList = new List<Appointment>();
+            DateTime dayIterator = lowerDateRange;
+            while (dayIterator < upperDateRange)
+            {
+                DateTime timeIterator = dayIterator;
+                while (timeIterator.Day == dayIterator.Day)
+                {
+                    if (DatesOverlap(dayIterator + lowerTimeRange, upperTimeRange - lowerTimeRange, timeIterator, appointmentLength))
+                    {
+                        Appointment appointment = new Appointment();
+                        appointment.Type = AppointmentType.Appointment;
+                        appointment.DoctorForeignKey = 0;
+                        appointment.PatientForeignKey = 0;
+                        appointment.Date = timeIterator;
+                        appointment.Time = appointmentLength;
+
+                        freeAppointmentList.Add(appointment);
+                    }
+                    timeIterator += appointmentLength;
+                }
+                dayIterator = dayIterator.AddDays(1);
+            }
+            return freeAppointmentList;
         }
 
         public void SaveSync()
--- WebAppCore/HospitalTests/PatientPortalTests/UnitTests/TestAppointmentService.cs
+++ WebAppCore/HospitalTests/PatientPortalTests/UnitTests/TestAppointmentService.cs
@@ -1,4 +1,5 @@
-﻿using Hospital.Schedule.Repository;
+﻿using Hospital.Schedule.Model;
+using Hospital.Schedule.Repository;
 using Hospital.Schedule.Service;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,17 @@ namespace HospitalTests.PatientPortalTests.UnitTests
             DateTime secondDate = new DateTime(2021, 6, 6, 11, 30, 0);
             TimeSpan secondTimeSpan = new TimeSpan(0, 1, 30, 0, 0);
             Assert.False(_service.DatesOverlap(firstDate, firstTimeSpan, secondDate, secondTimeSpan));
+        }
+
+        [Fact]
+        public void TestFreeAppointmentsListGenerated()
+        {
+            AppointmentService _service = new AppointmentService(new AppointmentRepository());
+            DateTime firstDate = new DateTime(2021, 6, 6, 0, 0, 0);
+            DateTime secondDate = new DateTime(2021, 6, 8, 23, 59, 59);
+            List<Appointment> freeAppointmentList = _service.GenerateFreeAppointmentList(firstDate, secondDate, new TimeSpan(12, 0, 0), new TimeSpan(14, 0, 0), new TimeSpan(1, 0, 0));
+            Assert.NotNull(freeAppointmentList);
+            Assert.Equal(6, freeAppointmentList.Count);
         }
     }
 }
