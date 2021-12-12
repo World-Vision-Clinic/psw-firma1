1bdd7af76f020da1e25abada200a987adc4b9070
ljuti21 <prodanovic.ra34.2018@uns.ac.rs>
2021-12-12 16:59:47 +0100

    fix: changed in memory repos in unit tests

--- WebAppCore/HospitalTests/EditorTests/UnitTests/MergeRoomsTest.cs
+++ WebAppCore/HospitalTests/EditorTests/UnitTests/MergeRoomsTest.cs
@@ -14,56 +14,57 @@ namespace HospitalTests.EditorTests.UnitTests
 {
     public class MergeRoomsTest
     {
+        private HospitalContext GetInMemoryRepository()
+        {
+            DbContextOptions<TestContext> options;
+            var builder = new DbContextOptionsBuilder<TestContext>();
+            builder.UseInMemoryDatabase("TestDb");
+            options = builder.Options;
+            HospitalContext hospitalContext = new TestContext(options);
+            hospitalContext.Database.EnsureDeleted();
+            hospitalContext.Database.EnsureCreated();
+            return hospitalContext;
+        }
+
         [Fact]
         public void merged_equipment_room_change()
         {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: "HospitalDB")
-                .Options;
+            HospitalContext context = GetInMemoryRepository();
 
-            using (var context = new HospitalContext(options))
-            {
-                Room r1 = new Room { Id = 88, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
-                Room r2 = new Room { Id = 888, FloorId = 1, Name = "OPERATING ROOM 2", DoctorId = -1, Purpose = "", X = 160, Y = 150, Height = 100, Width = 150, DoorX = 220, DoorY = 248, Vertical = false, Css = "room room-cadetblue", DoorExist = true };
-                context.Rooms.Add(r1);
-                context.Rooms.Add(r2);
-                Equipment eq1 = new Equipment { Id = 871, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 15, RoomId = 88 };
-                Equipment eq2 = new Equipment { Id = 872, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 4, RoomId = 888 };
-                Equipment eq3 = new Equipment { Id = 873, Name = "Infusion", Type = EquipmentType.DYNAMIC, Amount = 7, RoomId = 88 };
-                Equipment eq4 = new Equipment { Id = 874, Name = "Syringe", Type = EquipmentType.DYNAMIC, Amount = 23, RoomId = 888 };
-                context.AllEquipment.Add(eq1);
-                context.AllEquipment.Add(eq2);
-                context.AllEquipment.Add(eq3);
-                context.AllEquipment.Add(eq4);
+            Room r1 = new Room { Id = 88, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
+            Room r2 = new Room { Id = 888, FloorId = 1, Name = "OPERATING ROOM 2", DoctorId = -1, Purpose = "", X = 160, Y = 150, Height = 100, Width = 150, DoorX = 220, DoorY = 248, Vertical = false, Css = "room room-cadetblue", DoorExist = true };
+            context.Rooms.Add(r1);
+            context.Rooms.Add(r2);
+            Equipment eq1 = new Equipment { Id = 871, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 15, RoomId = 88 };
+            Equipment eq2 = new Equipment { Id = 872, Name = "Bandage", Type = EquipmentType.DYNAMIC, Amount = 4, RoomId = 888 };
+            Equipment eq3 = new Equipment { Id = 873, Name = "Infusion", Type = EquipmentType.DYNAMIC, Amount = 7, RoomId = 88 };
+            Equipment eq4 = new Equipment { Id = 874, Name = "Syringe", Type = EquipmentType.DYNAMIC, Amount = 23, RoomId = 888 };
+            context.AllEquipment.Add(eq1);
+            context.AllEquipment.Add(eq2);
+            context.AllEquipment.Add(eq3);
+            context.AllEquipment.Add(eq4);
+            context.SaveChanges();
 
-                context.SaveChanges();
-            }
-            using (var context = new HospitalContext(options))
-            {
-                EquipmentRepository eqRepository = new EquipmentRepository(context);
-                RoomRepository roomRepository = new RoomRepository(context);
-                RoomService roomService = new RoomService(roomRepository, eqRepository);
-                Room r1 = roomRepository.GetByID(88);
-                Room r2 = roomRepository.GetByID(888);
-                int newId = 699;
+            EquipmentRepository eqRepository = new EquipmentRepository(context);
+            RoomRepository roomRepository = new RoomRepository(context);
+            RoomService roomService = new RoomService(roomRepository, eqRepository);
 
-                roomService.mergeEquipment(r1.Id, r2.Id, newId);
+            Room rr1 = roomRepository.GetByID(88);
+            Room rr2 = roomRepository.GetByID(888);
+            int newId = 699;
 
-                eqRepository.GetRoomEquipemnts(r2.Id).ShouldBeEmpty();
-                eqRepository.GetRoomEquipemnts(r1.Id).ShouldBeEmpty();
-                eqRepository.GetRoomEquipemnts(newId).Count.ShouldBeEquivalentTo(4);
-                context.Dispose();
-            }
+            roomService.mergeEquipment(rr1.Id, rr2.Id, newId);
+
+            eqRepository.GetRoomEquipemnts(rr2.Id).ShouldBeEmpty();
+            eqRepository.GetRoomEquipemnts(rr1.Id).ShouldBeEmpty();
+            eqRepository.GetRoomEquipemnts(newId).Count.ShouldBeEquivalentTo(3);
+            context.Dispose();
         }
+        
         [Fact]
         public void same_name_equipment_merging()
         {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: "HospitalDB")
-                .Options;
-
-            using (var context = new HospitalContext(options))
-            {
+            HospitalContext context = GetInMemoryRepository();
 
                 Room r1 = new Room { Id = 399, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
                 Room r2 = new Room { Id = 3999, FloorId = 1, Name = "OPERATING ROOM 2", DoctorId = -1, Purpose = "", X = 160, Y = 150, Height = 100, Width = 150, DoorX = 220, DoorY = 248, Vertical = false, Css = "room room-cadetblue", DoorExist = true };
@@ -79,17 +80,15 @@ namespace HospitalTests.EditorTests.UnitTests
                 context.AllEquipment.Add(eq4);
 
                 context.SaveChanges();
-            }
-            using (var context = new HospitalContext(options))
-            {
+                      
                 EquipmentRepository eqRepository = new EquipmentRepository(context);
                 RoomRepository roomRepository = new RoomRepository(context);
                 RoomService roomService = new RoomService(roomRepository, eqRepository);
-                Room r1 = roomRepository.GetByID(399);
-                Room r2 = roomRepository.GetByID(3999);
+                Room rr1 = roomRepository.GetByID(399);
+                Room rr2 = roomRepository.GetByID(3999);
                 int newId = 3499;
 
-                roomService.mergeEquipment(r1.Id, r2.Id, newId);
+                roomService.mergeEquipment(rr1.Id, rr2.Id, newId);
                 foreach(Equipment e in eqRepository.GetAll())
                 {
                     if (e.Name.Equals("Bandage"))
@@ -99,19 +98,13 @@ namespace HospitalTests.EditorTests.UnitTests
                 }
                
                 context.Dispose();
-            }
         }
 
         [Fact]
         public void changin_total_number_of_roms_and_dimensions()
         {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: "HospitalDB")
-                .Options;
-            using (var context = new HospitalContext(options))
-            {
-
-                Room r1 = new Room { Id = 399, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
+            HospitalContext context = GetInMemoryRepository();
+            Room r1 = new Room { Id = 399, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
                 Room r2 = new Room { Id = 3999, FloorId = 1, Name = "OPERATING ROOM 2", DoctorId = -1, Purpose = "", X = 160, Y = 150, Height = 100, Width = 150, DoorX = 220, DoorY = 248, Vertical = false, Css = "room room-cadetblue", DoorExist = true };
                 context.Rooms.Add(r1);
                 context.Rooms.Add(r2);
@@ -125,17 +118,15 @@ namespace HospitalTests.EditorTests.UnitTests
                 context.AllEquipment.Add(eq4);
 
                 context.SaveChanges();
-            }
-            using (var context = new HospitalContext(options))
-            {
+
                 EquipmentRepository eqRepository = new EquipmentRepository(context);
                 RoomRepository roomRepository = new RoomRepository(context);
                 RoomService roomService = new RoomService(roomRepository, eqRepository);
-                Room r1 = roomRepository.GetByID(399);
-                Room r2 = roomRepository.GetByID(3999);
+                Room rr1 = roomRepository.GetByID(399);
+                Room rr2 = roomRepository.GetByID(3999);
 
 
-                roomService.mergeRooms(r1, r2, "New Room", "Room for personel rest");
+                roomService.mergeRooms(rr1, rr2, "New Room", "Room for personel rest");
                 List<Room> newRooms = roomService.getAll();
 
                 roomRepository.GetAll().Count.ShouldBeEquivalentTo(1);
@@ -150,8 +141,7 @@ namespace HospitalTests.EditorTests.UnitTests
                 }
                 
 
-                context.Dispose();
-            }
+                context.Dispose();           
         }
     }
 }
--- WebAppCore/HospitalTests/EditorTests/UnitTests/SplitRoomTest.cs
+++ WebAppCore/HospitalTests/EditorTests/UnitTests/SplitRoomTest.cs
@@ -14,30 +14,33 @@ namespace HospitalTests.EditorTests.UnitTests
 {
     public class SplitRoomTest
     {
+        private HospitalContext GetInMemoryRepository()
+        {
+            DbContextOptions<TestContext> options;
+            var builder = new DbContextOptionsBuilder<TestContext>();
+            builder.UseInMemoryDatabase("TestDb");
+            options = builder.Options;
+            HospitalContext hospitalContext = new TestContext(options);
+            hospitalContext.Database.EnsureDeleted();
+            hospitalContext.Database.EnsureCreated();
+            return hospitalContext;
+        }
+
         [Fact]
         public void change_of_dimensions_and_total_number_of_rooms()
         {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: "HospitalDB")
-                .Options;
-
-            using (var context = new HospitalContext(options))
-            {
+            HospitalContext context = GetInMemoryRepository();
                 Room r1 = new Room { Id = 88, FloorId = 1, Name = "OPERATING ROOM 1", DoctorId = -1, Purpose = "", X = 0, Y = 150, Height = 190, Width = 150, DoorX = 148, DoorY = 285, Vertical = true, Css = "room room-cadetblue", DoorExist = true };
                 context.Rooms.Add(r1);
-
-
                 context.SaveChanges();
-            }
-            using (var context = new HospitalContext(options))
-            {
+
                 EquipmentRepository eqRepository = new EquipmentRepository(context);
                 RoomRepository roomRepository = new RoomRepository(context);
                 RoomService roomService = new RoomService(roomRepository, eqRepository);
-                Room r1 = roomRepository.GetByID(88);
+                Room rr1 = roomRepository.GetByID(88);
                 int r1Width = r1.Width;
-                int r1Height = r1.Height;
-                roomService.splitRoom(r1, "New room 1", "Operations room", "New room 2", "Office");
+                int r1Height = rr1.Height;
+                roomService.splitRoom(rr1, "New room 1", "Operations room", "New room 2", "Office");
 
                 roomRepository.GetAll().Count.ShouldBe(2);
                 foreach(Room room in roomRepository.GetAll())
@@ -51,9 +54,7 @@ namespace HospitalTests.EditorTests.UnitTests
                         room.Width.ShouldBeLessThan(r1.Width);
                     }
                 }
-
                 context.Dispose();
-            }
             }
         
     }
