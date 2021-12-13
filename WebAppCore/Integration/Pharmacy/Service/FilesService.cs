4db2cd94136ae866ba307776acdd7b915e068faf
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2021-12-13 16:59:12 +0100

    feat: compression files using async task finished

--- WebAppCore/Integration/Pharmacy/Service/FilesService.cs
+++ WebAppCore/Integration/Pharmacy/Service/FilesService.cs
@@ -4,6 +4,7 @@ using Ionic.Zip;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Timers;
 
@@ -21,7 +22,7 @@ namespace Integration.Pharmacy.Service
 
         public void UpdateSpecification(Model.File dowloadedSpec)
         {
-            if(repository.GetByName(dowloadedSpec.Name) == null)
+            if (repository.GetByName(dowloadedSpec.Name) == null)
             {
                 repository.Save(dowloadedSpec);
             }
@@ -29,10 +30,41 @@ namespace Integration.Pharmacy.Service
 
         public static void CompressFiles(object sender, ElapsedEventArgs e)
         {
+            string[] filePaths = GetFilePathsForCompression();
+
+            if(filePaths.Length != 0)
+                Compress(filePaths, DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Year + ".zip");
+
+            DeleteFiles(filePaths);
+        }
+
+        public static string[] GetFilePathsForCompression()
+        {
+            string[] reports = GetFilesPathsFromDirectory(@"Reports\");
+            string[] prescriptions = GetFilesPathsFromDirectory(@"Prescriptions\");
+            string[] filePaths = { };
+
+            if (reports.Length != 0 && prescriptions.Length != 0)
+                filePaths = reports.Union(prescriptions).ToArray();
+            else if (reports.Length != 0)
+                filePaths = reports;
+            else if (prescriptions.Length != 0)
+                filePaths = prescriptions;
+            return filePaths;
+        }
+
+        public static string[] GetFilesPathsFromDirectory(string directory)
+        {
+            string[] pdfs = Directory.GetFiles(directory, "*.pdf", SearchOption.AllDirectories);
+            string[] txts = Directory.GetFiles(directory, "*.txt", SearchOption.AllDirectories);
+            string[] paths = pdfs.Union(txts).ToArray();
+            return paths;
+        }
+
+        public static void Compress(string[] filePaths, string zipName)
+        {
             using (ZipFile zip = new ZipFile())
             {
-                string[] filePaths = Directory.GetFiles(@"Specifications\", "*.pdf", SearchOption.AllDirectories);
-
                 foreach (string path in filePaths)
                 {
                     if (System.IO.File.GetLastWriteTime(path) < DateTime.Now)
@@ -41,10 +73,7 @@ namespace Integration.Pharmacy.Service
                     }
                 }
 
-                string zipName = DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Year + ".zip";
                 zip.Save(zipName);
-
-                DeleteFiles(filePaths);
             }
         }
 
---
+++ WebAppCore/IntegrationTests/UnitTests/FilesUnitTests.cs
@@ -0,0 +1,92 @@
+﻿using Integration.Pharmacy.Model;
+using Integration.Pharmacy.Service;
+using Integration_API.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntegrationTests.UnitTests
+{
+    public class FilesUnitTests
+    {
+        [Fact]  // interaction with Rebex Client
+        public void File_does_not_pdf_uploaded()
+        {
+            // Arrange
+            SftpHandler sftp = new SftpHandler();
+
+            // Act
+            File downloaded = sftp.DownloadSpecification($"/public/SomeFile.txt", "Specifications/SomeFile.txt");
+
+            // Assert
+            Assert.Null(downloaded);
+        }
+
+        [Fact]  // interaction with Rebex Client
+        public void File_is_uploaded()
+        {
+            // Arrange
+            SftpHandler sftp = new SftpHandler();
+
+            // Act
+            File downloaded = sftp.DownloadSpecification($"/public/Aspirin.pdf", "Specifications/Aspirin.pdf");
+
+            // Assert
+            Assert.NotNull(downloaded);
+
+        }
+
+        [Fact]
+        public void Check_get_file_paths_from_directory_when_there_is_two_files()
+        {
+            // Act
+            string[] paths = FilesService.GetFilesPathsFromDirectory(@"../../../../IntegrationTests/TestHaveFiles");
+
+            // Assert
+            Assert.Equal(2, paths.Length);
+        }
+
+        [Fact]
+        public void Check_get_file_paths_from_directory_when_there_is_no_files()
+        {
+            // Act
+            string[] paths = FilesService.GetFilesPathsFromDirectory(@"../../../../IntegrationTests/TestHaveNoFiles");
+
+            // Assert
+            Assert.Empty(paths);
+        }
+
+        [Fact]
+        public void Check_delete_files_method()
+        {
+            // Arrange
+            System.IO.File.Create(@"../../../../IntegrationTests/DeletedFiles/Test1.txt").Dispose();
+            System.IO.File.Create(@"../../../../IntegrationTests/DeletedFiles/Test2.txt").Dispose();
+            System.IO.File.Create(@"../../../../IntegrationTests/DeletedFiles/Test3.txt").Dispose();
+            string[] filePaths = { "../../../../IntegrationTests/DeletedFiles/Test1.txt",
+                                   "../../../../IntegrationTests/DeletedFiles/Test2.txt",
+                                   "../../../../IntegrationTests/DeletedFiles/Test3.txt"};
+
+            // Act
+            FilesService.DeleteFiles(filePaths);
+
+            // Assert
+            Assert.Empty(System.IO.Directory.GetFiles(@"../../../../IntegrationTests/DeletedFiles"));
+        }
+
+        [Fact]
+        public void Check_compress_files_method()
+        {
+            // Arrange
+            string source = "../../../../IntegrationTests/CompressFiles";
+            string dest = "../../../../IntegrationTests/" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Year + ".zip";
+
+            // Act
+            FilesService.Compress(System.IO.Directory.GetFiles(source), dest);
+
+            // Assert
+            Assert.True(System.IO.File.Exists(dest));
+        }
+    }
+}
