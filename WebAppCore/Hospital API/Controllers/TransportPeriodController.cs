6f69ad101e5de4745830b9a5e633a772f475c1ea
Petar Obradovic <ns.o.petar@gmail.com>
2021-12-14 07:11:17 +0100

    Feat: fix

--- WebAppCore/Hospital API/Controllers/TransportPeriodController.cs
+++ WebAppCore/Hospital API/Controllers/TransportPeriodController.cs
@@ -1,4 +1,5 @@
-﻿using Hospital.GraphicalEditor.Repository;
+﻿using Hospital.GraphicalEditor.Model;
+using Hospital.GraphicalEditor.Repository;
 using Hospital.GraphicalEditor.Repository.RepositoryInterfaces;
 using Hospital.GraphicalEditor.Service;
 using Hospital.MedicalRecords.Model;
@@ -24,42 +25,21 @@ namespace Hospital_API.Controllers
     {
         private RoomService roomService = new RoomService(new RoomRepository(new Hospital.SharedModel.HospitalContext()));
         private EquipmentService equipmentService = new EquipmentService(new EquipmentRepository(new Hospital.SharedModel.HospitalContext()));
-        private BuildingService buildingService = new BuildingService(new BuildingRepository(new Hospital.SharedModel.HospitalContext()));
         private FloorService floorService = new FloorService(new FloorRepository(new Hospital.SharedModel.HospitalContext()));
-        private FloorLabelService floorLabelService = new FloorLabelService(new FloorLabelRepository(new Hospital.SharedModel.HospitalContext()));
-        private MapPositionService mapPositionService = new MapPositionService(new MapPositionRepository(new Hospital.SharedModel.HospitalContext()));
-        private OutsideDoorService outsideDoorService = new OutsideDoorService(new OutsideDoorRepository(new Hospital.SharedModel.HospitalContext()));
-       
+
 
         [HttpGet]
-        public ActionResult<TransportationPeriodDTO> GetTransportationSuggestion([FromQuery] int buildingId, [FromQuery] double transportDurationInHours, [FromQuery] long startDateTimeStamp, [FromQuery] long endDateTimeStamp)
+        public ActionResult<DatePeriod> GetTransportationSuggestion([FromQuery] int buildingId, [FromQuery] double transportDurationInHours, [FromQuery] long startDateTimeStamp, [FromQuery] long endDateTimeStamp)
         {
-
-
-            DateTime startDate = DateTimeOffset.FromUnixTimeSeconds(startDateTimeStamp/1000).LocalDateTime;
+            DateTime startDate = DateTimeOffset.FromUnixTimeSeconds(startDateTimeStamp / 1000).LocalDateTime;
             DateTime endDate = DateTimeOffset.FromUnixTimeSeconds(endDateTimeStamp / 1000).LocalDateTime;
             startDate = new DateTime(startDate.Year, startDate.Month, startDate.Day, 8, 0, 0);
             endDate = new DateTime(endDate.Year, endDate.Month, endDate.Day, 8, 0, 0);
-            List<int> roomIds = roomService.getRoomIdsForBuilding(buildingId, floorService);
-            TransportationPeriodDTO tpDto = new TransportationPeriodDTO();
-            DateTime workingStartDate = startDate;
-            DateTime workingEndDate = workingStartDate.AddHours(transportDurationInHours);
-            List<Equipment> equipmentsInTransport = equipmentService.getAllInTransport(roomIds);
-            equipmentsInTransport = equipmentsInTransport.OrderBy(x => x.TransportStart).ToList<Equipment>();
-            foreach (Equipment equipment in equipmentsInTransport)
-            {
-                if(workingStartDate >= equipment.TransportStart && equipment.TransportEnd <= workingEndDate )
-                {
-                    workingStartDate = equipment.TransportEnd.AddMinutes(5);
-                    workingEndDate = workingStartDate.AddHours(transportDurationInHours);
-                    if (workingEndDate > endDate)
-                        return NotFound();
-                }
-            }
-            tpDto.startDate = workingStartDate;
-            tpDto.endDate = workingEndDate;
 
-            return tpDto;
+            DatePeriod datePeriod = equipmentService.SuggestTransportationPeriod(startDate, endDate, buildingId, floorService, roomService, equipmentService, transportDurationInHours);
+            if (datePeriod == null) return NotFound();
+
+            return datePeriod;
         }
 
 
@@ -69,17 +49,8 @@ namespace Hospital_API.Controllers
             try
             {
 
-
-                Equipment targetEquipment = equipmentService.getById(transportEquipmentDTO.TargetEqupmentId);
-                targetEquipment.Amount -= transportEquipmentDTO.Amount;
-                equipmentService.Update(targetEquipment);
-                Equipment equipmentInTransport = new Equipment();
-                equipmentInTransport.Name = targetEquipment.Name;
-                equipmentInTransport.InTransport = true;
-                equipmentInTransport.Amount = transportEquipmentDTO.Amount;
-                equipmentInTransport.TransportStart = convertTimestampToDatetime(transportEquipmentDTO.startDate);
-                equipmentInTransport.TransportEnd = convertTimestampToDatetime(transportEquipmentDTO.endDate);
-                equipmentInTransport.RoomId = transportEquipmentDTO.TargetRoomId;
+                equipmentService.reduceAmount(transportEquipmentDTO.TargetEqupmentId, transportEquipmentDTO.Amount);
+                Equipment equipmentInTransport = transportEquipmentDTO.getEquipment(equipmentService);
                 equipmentService.Create(equipmentInTransport);
             }
             catch
@@ -89,12 +60,6 @@ namespace Hospital_API.Controllers
             return Ok();
         }
 
-        private DateTime convertTimestampToDatetime(long timestamp)
-        {
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(timestamp).ToLocalTime();
-            return dtDateTime;
-        }
 
 
     }
--- WebAppCore/Hospital API/DTO/TransportEquipmentDTO.cs
+++ WebAppCore/Hospital API/DTO/TransportEquipmentDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hospital.RoomsAndEquipment.Model;
+using Hospital.RoomsAndEquipment.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +14,27 @@ namespace Hospital_API.DTO
         public long startDate { get; set; }
         public long endDate { get; set; }
         public int Amount { get; set; }
+
+        internal Equipment getEquipment(EquipmentService equipmentService)
+        {
+            Equipment equipment = equipmentService.getById(TargetEqupmentId);
+
+            equipment.InTransport = true;
+            equipment.Amount = Amount;
+            equipment.TransportStart = UnixTimeStampToDateTime(startDate);
+            equipment.TransportEnd = UnixTimeStampToDateTime(endDate);
+            equipment.RoomId = TargetRoomId;
+            return equipment;
+        }
+
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            if (unixTimeStamp > 1040242800000)
+                unixTimeStamp = unixTimeStamp / 1000;
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
