fd35465545d05176a1042a17b2c96c63275445c0
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2021-12-27 23:17:56 +0100

    feat:survey authorization

--- WebAppCore/Hospital API/Controllers/SurveyController.cs
+++ WebAppCore/Hospital API/Controllers/SurveyController.cs
@@ -1,3 +1,6 @@
+using Hospital.MedicalRecords.Model;
+using Hospital.MedicalRecords.Repository;
+using Hospital.MedicalRecords.Service;
 using Hospital.Schedule.Model;
 using Hospital.Schedule.Repository;
 using Hospital.Schedule.Service;
@@ -6,6 +9,7 @@ using Hospital_API.DTO;
 using Hospital_API.Mapper;
 using Hspital_API.Dto;
 using Hspital_API.Mapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -19,12 +23,17 @@ namespace Hospital_API
     {
         public SurveyService surveyService { get; set; }
         public AppointmentService _appointmentService { get; set; }
+        public PatientService _patientService { get; set; }
 
         public SurveyController()
         {
             surveyService = new SurveyService(new SurveyRepository(new HospitalContext()));
+            IAppointmentRepository appointmentRepository = new AppointmentRepository(new HospitalContext());
+            IPatientRepository patientRepository = new PatientRepository(new HospitalContext());
+            _patientService = new PatientService(patientRepository, appointmentRepository);
         }
 
+        [Authorize(Roles = "Manager")]
         [HttpGet("{id}")]
         public ActionResult<Survey> GetSurvey(int id)
         {
@@ -37,6 +46,7 @@ namespace Hospital_API
             return survey;
         }
 
+        [Authorize(Roles = "Patient, Manager")]
         [HttpGet]
         public ActionResult<IEnumerable<QuestionDTO>> GetQuestions()
         {
@@ -48,18 +58,38 @@ namespace Hospital_API
             return Ok(dtoList);
         }
 
+        [Authorize(Roles = "Manager")]
         [HttpGet("answered_questions_breakdown")]
         public ActionResult<IEnumerable<SurveyBreakdownDTO>> GetAnsweredQuestionsBreakdown()
         {
             return SurveyBreakdownMapper.AllSurveysToSurveyBreakdownDTO(surveyService.GetAllAnsweredQuestions());
         }
 
+        [Authorize(Roles = "Patient")]
         [HttpPost("{id}")]
         public ActionResult<Survey> PostSurveyQuestions([FromBody] List<QuestionDTO> questions, int id)
         {
             Survey newSurvey = new Survey();
             newSurvey.CreationDate = System.DateTime.Now;
             newSurvey.IdAppointment = id;
+
+            Patient patient = getCurrentPatient();
+
+            List<Appointment> appointments = _appointmentService.GetByPatientId(patient.Id);
+
+            bool found = false;
+            foreach (Appointment a in appointments)
+            {
+                if (a.Id == id)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                return Unauthorized();
+            }
 
             foreach (QuestionDTO dtos in questions)
             {
@@ -76,6 +106,13 @@ namespace Hospital_API
                 surveyService.AddAnswer(QuestionMapper.QuestionDTOToAnswer(dtos, IdSurvey));
             }
             return Ok(newSurvey);
+        }
+
+        private Patient getCurrentPatient()
+        {
+            string username = User.FindFirst("username")?.Value;
+            Patient patient = _patientService.FindByUserName(username);
+            return patient;
         }
     }
 }
