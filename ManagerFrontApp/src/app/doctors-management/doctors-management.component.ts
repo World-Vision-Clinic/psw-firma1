1a467d5e2d3ae471437266a9a28dd9722e53a974
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-01-07 18:18:39 +0100

    feat: initial table for vacations

--- ManagerFrontApp/src/app/doctors-management/doctors-management.component.ts
+++ ManagerFrontApp/src/app/doctors-management/doctors-management.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorsManagementComponent implements OnInit {
 
+  initialTableBox = true;
+  vacationTableBox = false;
+  
   constructor() { }
 
   ngOnInit(): void {
