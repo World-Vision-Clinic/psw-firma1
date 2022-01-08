f4c13ddad30f6024eac5b10a7a42c863e0ce2e79
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2022-01-08 15:53:12 +0100

    fix: changed front errors

--- ManagerFrontApp/src/app/manager-integration-front-app/tender-selection/tender-selection.component.ts
+++ ManagerFrontApp/src/app/manager-integration-front-app/tender-selection/tender-selection.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-tender-selection',
   templateUrl: './tender-selection.component.html',
   styleUrls: ['./tender-selection.component.css']
 })
+
+@Injectable({providedIn:'root'})
 export class TenderSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  TenderList:any=[];
 
   ngOnInit(): void {
+    this.getTenders();
+  }
+
+  getTenders(){
+    return this.http.get<any>("http://localhost:43818/Tender").subscribe(data=>{
+          this.TenderList=data;
+          console.log(this.TenderList);
+    });
   }
 
 }
