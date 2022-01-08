ee74b96c7b7147787f20b465486af4e8a6ce44bc
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2022-01-08 15:46:53 +0100

    feat: added frontend for tender selection

---
+++ ManagerFrontApp/src/app/manager-integration-front-app/tender-selection/tender-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tender-selection',
+  templateUrl: './tender-selection.component.html',
+  styleUrls: ['./tender-selection.component.css']
+})
+export class TenderSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
