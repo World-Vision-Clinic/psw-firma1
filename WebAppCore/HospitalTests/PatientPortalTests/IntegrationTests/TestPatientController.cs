85e6a8cfad9e9520221358d733d5d752fa41b429
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2022-01-09 15:02:16 +0100

    fix: Partially fixed preferred doctor test error

--- WebAppCore/HospitalTests/PatientPortalTests/IntegrationTests/TestPatientController.cs
+++ WebAppCore/HospitalTests/PatientPortalTests/IntegrationTests/TestPatientController.cs
@@ -95,20 +95,17 @@ namespace HospitalTests.PatientPortalTests.IntegrationTests
         [Fact]
         public void Test_preferred_doctor_found()
         {
-            Patient patient = new Patient(4, "Marko123", "123", new FullName("Marko", "Markovic"), "markomarkovic@gmail.com", false, Gender.Male, "1637597",
-                DateTime.Now, new Residence("Serbia", "TestAddress", "TestCity"), "063115111", 10, 80, 180, BloodType.A, false, new List<Appointment>());
             Doctor doctor = new Doctor()
             {
                 Id = 10,
                 FirstName = "MarkovDoktorIme",
                 LastName = "MarkovDoktorPrezime"
             };
-            _patientRepository.AddPatient(patient);
             _doctorRepository.AddDoctor(doctor);
 
-            /*var response = _patientsController.GetPatient(4);
+            var response = _patientsController.GetPatient();
 
-            Assert.Equal("MarkovDoktorIme MarkovDoktorPrezime", response.Value.PreferedDoctorName);*/
+            Assert.Equal("MarkovDoktorIme MarkovDoktorPrezime", response.Value.PreferedDoctorName);
         }
 
         [Fact]
