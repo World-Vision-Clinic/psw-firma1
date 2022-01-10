e8db3883a28cbc21ab2f16b0ef3dadf3d90e34ee
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2022-01-10 21:15:10 +0100

    feat: added test for creating tender

--- ManagerFrontApp/src/app/app.module.ts
+++ ManagerFrontApp/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuildingsMapComponent } from './buildings-map/buildings-map.component';
@@ -8,7 +7,7 @@ import { Hospital1Component } from './hospital1/hospital1.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { RoomComponent } from './room/room.component';
 import { FormsModule } from '@angular/forms';
-//import { NgSelectModule } from '@ng-select/ng-select';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { PharmacyRegistrationComponent } from './pharmacy-registration/pharmacy-registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ManagerIntegrationFrontAppComponent } from './manager-integration-front-app/manager-integration-front-app.component';
@@ -17,18 +16,15 @@ import { OverviewObjectionsRepliesComponent } from './manager-integration-front-
 import { ReactiveFormsModule } from '@angular/forms';
 import { ObjectionFormPageComponent } from './manager-integration-front-app/objection-form-page/objection-form-page.component';
 import { ManagerFeedbackViewComponent } from './manager-feedback-view/manager-feedback-view.component';
-//import { PharmaciesComponent } from './manager-integration-front-app/pharmacies/pharmacies.component';
-//import { NewsComponent } from './manager-integration-front-app/news/news.component';
+import { PharmaciesComponent } from './manager-integration-front-app/pharmacies/pharmacies.component';
+import { NewsComponent } from './manager-integration-front-app/news/news.component';
 import { ViewSurveyResultsComponent } from './view-survey-results/view-survey-results.component';
-//import { MedicineConsumptionComponent } from './manager-integration-front-app/medicine-consumption/medicine-consumption.component';
+import { MedicineConsumptionComponent } from './manager-integration-front-app/medicine-consumption/medicine-consumption.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BlockPatientsComponent } from './block-patients/block-patients.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-//import { ToastrModule } from 'ngx-toastr';
-//import { GetSpecificationComponent } from './manager-integration-front-app/get-specification/get-specification.component';
-//import { ViewFilesComponent } from './manager-integration-front-app/view-files/view-files.component';
 import { ToastrModule } from 'ngx-toastr';
 import { GetSpecificationComponent } from './manager-integration-front-app/get-specification/get-specification.component';
 import { ViewFilesComponent } from './manager-integration-front-app/view-files/view-files.component';
@@ -56,16 +52,12 @@ import { DoctorsManagementComponent } from './doctors-management/doctors-managem
     ObjectionFormPageComponent,
     ManagerFeedbackViewComponent,
     ManagerIntegrationFrontAppComponent,
-    //PharmaciesComponent,
+    PharmaciesComponent,
     ViewSurveyResultsComponent,
     BlockPatientsComponent,
     DashboardComponent,
-    //MedicineConsumptionComponent,
-    //NewsComponent,
-    //GetSpecificationComponent,
-    //ViewFilesComponent
-    //MedicineConsumptionComponent,
-    //NewsComponent,
+    MedicineConsumptionComponent,
+    NewsComponent,
     GetSpecificationComponent,
     ViewFilesComponent,
     LoginComponent,
@@ -82,11 +74,10 @@ import { DoctorsManagementComponent } from './doctors-management/doctors-managem
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    //NgSelectModule,
+    NgSelectModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatNativeDateModule,
-    //ToastrModule.forRoot()
     ToastrModule.forRoot(),
     BrowserModule,
     NgChartsModule,
