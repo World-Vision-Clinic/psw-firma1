f116619473b2a88b94d3fca8f6ef5e36657d07d8
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2022-01-10 20:45:54 +0100

    refactor: Changed feedback constructor

--- WebAppCore/Hospital/MedicalRecords/Model/Feedback.cs
+++ WebAppCore/Hospital/MedicalRecords/Model/Feedback.cs
@@ -9,18 +9,22 @@ namespace Hospital.MedicalRecords.Model
 {
     public class Feedback
     {
-        public int Id { get; set; }
-        public string Content { get; set; }
-        public bool IsPublic { get; set; }
-        public bool IsPublishable { get; set; }
-        public bool IsAnonymous { get; set; }
-        public DateTime Date { get; set; }
+        public int Id { get; private set; }
+        public string Content { get; private set; }
+        public bool IsPublic { get; private set; }
+        public bool IsPublishable { get; private set; }
+        public bool IsAnonymous { get; private set; }
+        public DateTime Date { get; private set; }
         public string UserName { get; set; }
 
-        public Feedback()
+        public Feedback(string content, bool isPublic, bool isPublishable, bool isAnonymous, DateTime date, string username)
         {
-            this.Date = DateTime.Now;
-            this.UserName = "User";
+            this.Content = content;
+            this.IsPublic = isPublic;
+            this.IsPublishable = isPublishable;
+            this.IsAnonymous = isAnonymous;
+            this.Date = date;
+            this.UserName = username;
         }
     }
 }
