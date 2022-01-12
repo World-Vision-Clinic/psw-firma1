e8d4767ddd7b6ae8ae32598fd9113d5464d562b2
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2022-01-12 19:43:48 +0100

    feat: added test for creating tender

---
+++ WebAppCore/IntegrationTests/EndToEndTests/CreatingTenderTest.cs
@@ -0,0 +1,118 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntegrationTests.EndToEndTests
+{
+    public class CreatingTenderTest : IDisposable
+    {
+        private readonly IWebDriver driver;
+        private Pages.TenderPage tenderPage;
+        public CreatingTenderTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");    // disable notifications
+
+            driver = new ChromeDriver(options);
+
+
+            tenderPage = new Pages.TenderPage(driver);      // create TenderPage
+            tenderPage.Navigate();
+
+        }
+
+        [Fact]
+        public void Create_tender()
+        {
+            tenderPage.InsertTitle("TestTitle");
+            tenderPage.InsertDescription("TestDescription");
+            tenderPage.InsertMedicineName("TestMedicine");
+            tenderPage.InsertDosage("100");
+            tenderPage.InsertQuantity("10");
+            tenderPage.AddMedicine();
+
+            tenderPage.Wait();
+            tenderPage.AddTender();
+
+            tenderPage.WaitForAlertDialog();
+            tenderPage.AcceptAlert();
+            tenderPage.WaitForAlertDialog();
+
+
+            Assert.Equal(tenderPage.GetDialogMessage(), Pages.TenderPage.SuccessfulOrderingMessage);
+        }
+        [Fact]
+        public void Forget_to_insert_title()
+        {
+            tenderPage.InsertDescription("TestDescription");
+            tenderPage.InsertMedicineName("TestMedicine");
+            tenderPage.InsertDosage("100");
+            tenderPage.InsertQuantity("10");
+            tenderPage.AddMedicine();
+
+            tenderPage.Wait();
+            tenderPage.AddTender();
+
+            tenderPage.WaitForAlertDialog();
+
+
+            Assert.Equal(tenderPage.GetDialogMessage(), Pages.TenderPage.UnsuccessfulOrderingMessage);
+        }
+        [Fact]
+        public void Forget_to_insert_description()
+        {
+            tenderPage.InsertTitle("TestTitle");
+            tenderPage.InsertMedicineName("TestMedicine");
+            tenderPage.InsertDosage("100");
+            tenderPage.InsertQuantity("10");
+            tenderPage.AddMedicine();
+
+            tenderPage.Wait();
+            tenderPage.AddTender();
+
+            tenderPage.WaitForAlertDialog();
+
+
+            Assert.Equal(tenderPage.GetDialogMessage(), Pages.TenderPage.UnsuccessfulOrderingMessage);
+        }
+        [Fact]
+        public void Forget_to_insert_medicine()
+        {
+            tenderPage.InsertTitle("TestTitle");
+            tenderPage.InsertDescription("TestDescription");
+
+            tenderPage.Wait();
+            tenderPage.AddTender();
+            tenderPage.WaitForAlertDialog();
+
+
+            Assert.Equal(tenderPage.GetDialogMessage(), Pages.TenderPage.UnsuccessfulOrderingMessage);
+        }
+        [Fact]
+        public void Forget_to_insert_all()
+        {
+            tenderPage.Wait();
+            tenderPage.AddTender();
+
+            tenderPage.WaitForAlertDialog();
+
+
+            Assert.Equal(tenderPage.GetDialogMessage(), Pages.TenderPage.UnsuccessfulOrderingMessage);
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
