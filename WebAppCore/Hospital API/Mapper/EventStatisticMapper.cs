f541cd642075cfe6eabe8a5884deeeb2681a565b
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2022-01-19 18:41:29 +0100

    feat: 4Step use times statistic backend

--- WebAppCore/Hospital API/Mapper/EventStatisticMapper.cs
+++ WebAppCore/Hospital API/Mapper/EventStatisticMapper.cs
@@ -15,6 +15,7 @@ namespace Hospital_API.Mapper
         {
             List<EventStatisticDTO> allStatistics = new List<EventStatisticDTO>();
             allStatistics.Add(GetSuccessful4StepAttempts());
+            allStatistics.Add(GetUseTimes());
             return allStatistics;
         }
 
@@ -23,19 +24,46 @@ namespace Hospital_API.Mapper
             EventService eventService = new EventService(new EventRepository());
             EventStatisticDTO statistic = new EventStatisticDTO("Successful Attempts");
             List<Event> allEvents = eventService.GetAll();
-            int startCount = allEvents
+            float startCount = allEvents
                 .Where(p => String.Equals(p.Name, "START"))
                 .Count();
-            int endCount = allEvents
+            float endCount = allEvents
                 .Where(p => String.Equals(p.Name, "END"))
                 .Count();
             float successRate = 0;
             if(startCount != 0)
             {
-                successRate = (float)(endCount / startCount) * 100;
+                successRate = (endCount / startCount) * 100;
             }
             statistic.Data.Add(new EventStatisticDataPair("Successful", successRate));
             statistic.Data.Add(new EventStatisticDataPair("Unsuccessful", (100 - successRate)));
+            return statistic;
+        }
+
+        private static EventStatisticDTO GetUseTimes()
+        {
+            EventService eventService = new EventService(new EventRepository());
+            EventStatisticDTO statistic = new EventStatisticDTO("Use Time");
+            List<Event> allEndEvents = eventService.GetAll().Where(p => String.Equals(p.Name, "END")).ToList();
+            double averageUseSeconds = allEndEvents.Average(p => p.TimeDifference.TotalSeconds);
+            double maxUseSeconds = allEndEvents.Max(p => p.TimeDifference.TotalSeconds) + 0.1;
+            double[] cutoffPoints = new double[5];
+            cutoffPoints[0] = 0;
+            cutoffPoints[1] = averageUseSeconds / 2;
+            cutoffPoints[2] = averageUseSeconds;
+            cutoffPoints[3] = (averageUseSeconds + maxUseSeconds) / 2;
+            cutoffPoints[4] = maxUseSeconds;
+            for (int i = 0; i < 5; i++)
+            {
+                cutoffPoints[i] = Math.Truncate(cutoffPoints[i] * 100) / 100;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int eventCount = allEndEvents
+                    .Where(p => p.TimeDifference.TotalSeconds > cutoffPoints[i] && p.TimeDifference.TotalSeconds <= cutoffPoints[i + 1])
+                    .Count();
+                statistic.Data.Add(new EventStatisticDataPair(cutoffPoints[i].ToString() + "-" + cutoffPoints[i+1].ToString(), eventCount));
+            }
             return statistic;
         }
     }
