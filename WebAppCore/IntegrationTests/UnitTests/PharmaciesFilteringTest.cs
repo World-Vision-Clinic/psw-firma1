53ac43b66109e6103ae6be57b2e2c43aff23afc5
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2022-03-13 14:38:20 +0100

    refactor: first tests

--- WebAppCore/IntegrationTests/UnitTests/PharmaciesFilteringTest.cs
+++ WebAppCore/IntegrationTests/UnitTests/PharmaciesFilteringTest.cs
@@ -15,24 +15,24 @@ namespace IntegrationTests.UnitTests
         [Fact]
         public void GettingAllPharmaciesTest()
         {
-            var stubRepository = new Mock<IPharmaciesRepository>();
-            var pharmacies = new List<PharmacyProfile>();
+            service = new PharmaciesService(CreateStubRepository());
 
-            PharmacyProfile pp1 = new PharmacyProfile("Benu", "dsadkasj", "localhost:8080", ProtocolType.GRPC, "Nemanjica", "Beograd");
-            PharmacyProfile pp2 = new PharmacyProfile("Jankovic", "dsadkasj", "localhost:8080", ProtocolType.GRPC, "Nemanjica", "Nevesinje");
-
-            pharmacies.Add(pp1);
-            pharmacies.Add(pp2);
-
-            stubRepository.Setup(m => m.GetAll()).Returns(pharmacies);
-
-            service = new PharmaciesService(stubRepository.Object);
             List<PharmacyProfile> allPharmacies = service.GetAll();
+
             Assert.Equal(2, allPharmacies.Count);
         }
 
         [Fact]
         public void FilteringByExistingCityTest()
+        {
+            service = new PharmaciesService(CreateStubRepository());
+            
+            List<PharmacyProfile> foundedPharmacies = service.GetFiltered("Beograd");
+            
+            Assert.NotEmpty(foundedPharmacies);
+        }
+
+        private static IPharmaciesRepository CreateStubRepository()
         {
             var stubRepository = new Mock<IPharmaciesRepository>();
             var pharmacies = new List<PharmacyProfile>();
@@ -44,12 +44,10 @@ namespace IntegrationTests.UnitTests
             pharmacies.Add(pp2);
 
             stubRepository.Setup(m => m.GetAll()).Returns(pharmacies);
-            service = new PharmaciesService(stubRepository.Object);
-            
-            List<PharmacyProfile> foundedPharmacies = service.GetFiltered("Beograd");
-            
-            Assert.NotEmpty(foundedPharmacies);
+
+            return stubRepository.Object;
         }
+
         [Fact]
         public void FilteringByUnexistingCityTest()
         {
